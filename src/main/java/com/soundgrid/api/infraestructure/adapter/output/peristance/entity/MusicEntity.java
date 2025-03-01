60580522c88c2797d23f0516fb4a4bfd97605557
brunamark <bruninha.markowisk@hotmail.com>
2025-03-01 20:48:54 -0300

    add: music entity and mapper adapter

---
+++ src/main/java/com/soundgrid/api/infraestructure/adapter/output/peristance/entity/MusicEntity.java
@@ -0,0 +1,120 @@
+package com.soundgrid.api.infraestructure.adapter.output.peristance.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.soundgrid.api.domain.types.Genre;
+
+public class MusicEntity {
+
+
+    private Long id;
+    private String title;
+    private String artist;
+    private String album;
+    private Genre genre;             
+    private LocalDate releaseDate;
+    private int duration;
+    private String filePath;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public MusicEntity() {
+    }
+    public MusicEntity(Long id, String title, String artist, String album, Genre genre,
+                       LocalDate releaseDate, int duration, String filePath,
+                       LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.filePath = filePath;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
+
